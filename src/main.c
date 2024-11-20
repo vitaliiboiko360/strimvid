3e99bd3d911b223dc9d00c19e20fcd5fe66d4db3
vitalii boiko <vukelan@gmail.com>
2024-11-20 23:43:45 +0200

    add server from examples

--- src/main.c
+++ src/main.c
@@ -5,22 +5,44 @@ int
 main (int   argc,
       char *argv[])
 {
-  const gchar *nano_str;
-  guint major, minor, micro, nano;
+GMainLoop *loop;
+  GstRTSPServer *server;
+  GstRTSPMountPoints *mounts;
+  GstRTSPMediaFactory *factory;
 
   gst_init (&argc, &argv);
 
-  gst_version (&major, &minor, &micro, &nano);
+  loop = g_main_loop_new (NULL, FALSE);
 
-  if (nano == 1)
-    nano_str = "(CVS)";
-  else if (nano == 2)
-    nano_str = "(Prerelease)";
-  else
-    nano_str = "";
+  /* create a server instance */
+  server = gst_rtsp_server_new ();
 
-  printf ("This program is linked against GStreamer %d.%d.%d %s\n",
-          major, minor, micro, nano_str);
+  /* get the mount points for this server, every server has a default object
+   * that be used to map uri mount points to media factories */
+  mounts = gst_rtsp_server_get_mount_points (server);
+
+  /* make a media factory for a test stream. The default media factory can use
+   * gst-launch syntax to create pipelines. 
+   * any launch line works as long as it contains elements named pay%d. Each
+   * element with pay%d names will be a stream */
+  factory = gst_rtsp_media_factory_new ();
+  gst_rtsp_media_factory_set_launch (factory,
+      "( videotestsrc is-live=1 ! x264enc ! rtph264pay name=pay0 pt=96 )");
+
+  gst_rtsp_media_factory_set_shared (factory, TRUE);
+
+  /* attach the test factory to the /test url */
+  gst_rtsp_mount_points_add_factory (mounts, "/test", factory);
+
+  /* don't need the ref to the mapper anymore */
+  g_object_unref (mounts);
+
+  /* attach the server to the default maincontext */
+  gst_rtsp_server_attach (server, NULL);
+
+  /* start serving */
+  g_print ("stream ready at rtsp://127.0.0.1:8554/test\n");
+  g_main_loop_run (loop);
 
   return 0;
 }
